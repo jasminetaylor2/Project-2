e02dfe4111ed998045cb3d2fe8640f54f33ce21d
jasminetaylor2 <51241456+jasminetaylor2@users.noreply.github.com>
2019-06-21 15:38:45 -0400

    updated notes

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const itemsPerPage = 10;                                 //represents the number
 console.log(studentList);
 
 //this function should hide all students expect 10 you want on given page
- const showPage = (list, page) => {
+const showPage = (list, page) => {
    let startIndex = (page * itemsPerPage) - itemsPerPage; //starting point for the index '0'
    let endIndex = page * itemsPerPage;                   //ending point for index '10' 
    for (let i = 0; i < list.length; i += 1) {                 //for loop to run thru entire student list items'li'
@@ -22,70 +22,55 @@ console.log(studentList);
          list[i].style.display = "none";            //these manipulate the list items on the page using CSS 'display'to hide the element 
       }
    }
-} 
+}
 
-showPage(studentList, 1);
+showPage(studentList, 1);                          // show the firrst 10 student on list
 
 
 
 const appendPageLinks = (list) => {
-   var numOfPages = Math.ceil(list.length/itemsPerPage);        //formula to determine how many pages let numOfPages 
-   const pageClass = document.querySelector('.page');           //variable that hold the div with the name '.page'
-   const createDiv = document.createElement('div');             //creates div element 
+   let numOfPages = Math.ceil(list.length / itemsPerPage);        //formula to determine how many pages let numOfPages 
+   let pageClass = document.querySelector('.page');           //variable that hold the div with the name '.page'
+   let createDiv = document.createElement('div');             //creates div element 
    createDiv.className = "pagination";                          //gives the new div the class name 'pagination'
    pageClass.appendChild(createDiv);                            // appends to page div
    let ul = document.createElement('ul');                       //create ul variable
    createDiv.appendChild(ul);                                   //append ul to div element with pagination link 
 
 
-    for (let i = 1; i <= numOfPages; i += 1) {                // loop over every page link
+   for (let i = 1; i <= numOfPages; i += 1) {                // loop over every page link
       let li = document.createElement('li');                  // creates a list item
-      let a = document.createElement('a'); 
-                         // creates an anchor elements
-      li.appendChild(a);
-      ul.appendChild(li);
-   
-      a.textContent = i;
-      a.href = '#';
+      let a = document.createElement('a');                      // creates an anchor elements
 
-            if (i === 1)
-            {
-               a.className = 'pagination active';
-            }
+      li.appendChild(a);                           //append new created anchor element to be nested within li
+      ul.appendChild(li);                          // nest list elements within unorderlist
 
-      a.addEventListener('click', (event) => {
-         const clicked = event.target;
-         const pagLinks = document.querySelectorAll('.pagination');
-         showPage(list, i);
+      a.textContent = i;                           // sets the text content of link to equal the index value
+      a.href = '#';                                // set the text of the anchor element 
 
-         for (let index = 0; index < pagLinks.length; index++) {
+      if (i === 1) {                               // sets the page to 1
+         a.className = 'pagination active';        //set the anchor class name
+      }
+
+      a.addEventListener('click', (event) => {       // adds an event listner to the anchor tag
+         let clicked = event.target;             // sets the variable of click to the event target
+         let pagLinks = document.querySelectorAll('.pagination'); //sets the vairable to all the elements with pagination class name
+         showPage(list, i);                              // calls the function showPage  
+
+         for (let index = 0; index < pagLinks.length; index++) { // loops over pagination links
             let theLinks = pagLinks[index];
-            theLinks.setAttribute('class', 'pagination');      //when not clicked makes it unactive
+            theLinks.setAttribute('class', 'pagination');      //when not clicked makes pagination button unactive
          }
          clicked.setAttribute('class', "pagination active");   //when clicked it makes active
 
-         });
-      }
+      });
    }
+}
 
-   appendPageLinks(studentList);
-
-   
-      
-   //    let page1 = document.getElementsByTagName(a).innerHTML + "<a href = '#' >numOfPages </a>";
-   //    document.getElementsByTagName(a).innerHTML += "<a href = '#' > numOfPages </a>";
-   //    document.getElementsByTagName(a).innerHTML += "<a href = '#' > 'numOfPages </a>";
-   //    document.getElementsByTagName(a).innerHTML += "<a href = '#' > numOfPages </a>";
-   //    document.getElementsByTagName(a).innerHTML += "<a href = '#' > numOfPages </a>";
-
-   //    page1.className = 'active';
-   //}      //for(let p = 0; p < list.length; p ++ ){
-    //  document.getElementsByTagName(a).innerHTML += "<a href = '#' >2</a> ;
+appendPageLinks(studentList);                   //calls the funtion to append the page
 
 
 
 
 
-  //  createDiv.getElementsByClassName('.pagination').appendChild(ul);
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
